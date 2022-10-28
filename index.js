2c87f38da7a26022aba47b8fde18d43acd39ee6c
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2022-10-28 20:10:07 -0300

    Deploy

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+
+    const {data} = await axios('https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json');
+    console.log(data)
+
+    res.json({
+        Data: data.dt,
+        Hora: data.ht,
+        SecoesTotalizadas: data.pst + '%',
+        Abstencao: data.pa + '%',
+        LULA: data.cand[0].pvap +'%', 
+        BOLSONARO: data.cand[1].pvap +'%'
+    })
+});
+
+app.listen(3030, () => {
+    console.log(`servidor disponível na porta 3030`)
+})
