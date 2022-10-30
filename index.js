d1a241b6d2caa60fa279cc4e98dc964616f134fd
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2022-10-30 11:40:33 -0300

    ajuste data

--- index.js
+++ index.js
@@ -7,17 +7,16 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', async (req, res) => {
 
     const {data} = await axios('https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json');
-    console.log(data)
 
     res.json({
-        Data: data.dt,
-        Hora: data.ht,
+        Data: data.dg,
+        Hora: data.hg,
         SecoesTotalizadas: data.pst + '%',
         Abstencao: data.pa + '%',
         LULA: data.cand[0].pvap +'%',
         Votos1: data.cand[0].vap,
         BOLSONARO: data.cand[1].pvap +'%',
-        Votos2: data.cand[1].vap,
+        Votos2: data.cand[1].vap
     })
 });
 
