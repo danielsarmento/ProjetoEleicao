7a4cc6e795f718f92392ccde09f5f3bc38099ffc
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2022-10-30 12:15:36 -0300

    ajuste gov

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', async (req, res) => {
 
     const {data} = await axios('https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json');
-    const {dataGov} = await axios('https://resultados.tse.jus.br/oficial/ele2022/547/dados-simplificados/pb/pb-c0003-e000547-r.json');
+    const dataGov = await axios('https://resultados.tse.jus.br/oficial/ele2022/547/dados-simplificados/pb/pb-c0003-e000547-r.json');
 
     res.json({
         Data: data.dg,
@@ -19,16 +19,17 @@ app.get('/', async (req, res) => {
         BOLSONARO: data.cand[1].pvap +'%',
         Votos2: data.cand[1].vap,
 
-        SecoesTotalizadasPB: dataGov.pst + '%',
-        AbstencaoPB: dataGov.pa + '%',
-        Joao: dataGov.cand[0].pvap +'%',
-        Votos1PB: dataGov.cand[0].vap,
-        Pedro: dataGov.cand[1].pvap +'%',
-        Votos2PB: dataGov.cand[1].vap
+        SecoesTotalizadasPB: dataGov.data.pst + '%',
+        AbstencaoPB: dataGov.data.pa + '%',
+        Joao: dataGov.data.cand[0].pvap +'%',
+        Votos1PB: dataGov.data.cand[0].vap,
+        Pedro: dataGov.data.cand[1].pvap +'%',
+        Votos2PB: dataGov.data.cand[1].vap
 
 
     })
 });
+
 
 app.listen(3030, () => {
     console.log(`servidor disponível na porta 3030`)
